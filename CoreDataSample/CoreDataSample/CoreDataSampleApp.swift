a1b811212571319f49cab6da148989cb9d66bcca
ioannispechlivanis <pechlivanis.i@web.de>
2024-08-13 08:01:21 +0200

    init commit

---
+++ CoreDataSample/CoreDataSample/CoreDataSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataSampleApp.swift
+//  CoreDataSample
+//
+//  Created by Ioannis Pechlivanis on 13.08.24.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

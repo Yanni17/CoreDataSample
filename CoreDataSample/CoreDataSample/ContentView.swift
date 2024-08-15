8166542af20ba6787d8f5eebc34afcbcd45e2f27
Nick Podratz <nick.podratz@syntax-institut.de>
2024-08-15 10:27:54 +0200

    Add tabbar navigation

--- CoreDataSample/CoreDataSample/ContentView.swift
+++ CoreDataSample/CoreDataSample/ContentView.swift
@@ -9,7 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        StudentListView()
+        TabView {
+            StudentListView()
+                .tabItem {
+                    Label(
+                        title: { Text("Students") },
+                        icon: { Image(systemName: "studentdesk") }
+                    )
+                }
+            CoursesListView()
+                .tabItem {
+                    Label(
+                        title: { Text("Courses") },
+                        icon: { Image(systemName: "book.closed.fill") }
+                    )
+                }
+        }
     }
 }
 
